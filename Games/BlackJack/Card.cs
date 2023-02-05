f1c5241d57f3e2f425262e8c5337919787ac41e8
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-05 18:24:23 +0300

    update

---
+++ Games/BlackJack/Card.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Card	
+{
+    public Card(string name, int value, CardType type)
+        => (Name, Value, Type) = (name, value, type);
+
+    public string Name { get; set; }
+    public int Value { get; set; }
+    public CardType Type { get; set; }
+}
