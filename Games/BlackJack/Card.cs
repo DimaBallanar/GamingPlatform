7deed52119b6a808c9080a9ceab6c8ba5f6f31b2
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-06 14:32:51 +0300

    update

--- Games/BlackJack/Card.cs
+++ Games/BlackJack/Card.cs
@@ -5,13 +5,13 @@ public class Card
 {
     public string Name { get; set; }
     public int Value { get; set; }
-    public CardTypes Type { get; set; }
+    //public CardTypes Type { get; set; }
     public CardEnum CardEnum { get; set; }
-    public Card(string name, int value, CardTypes type,CardEnum card)
+    public Card(string name, int value,/* CardTypes type,*/CardEnum card)
     {
         Name = name;
         Value = value;
-        Type = type;
+        //Type = type;
         CardEnum = card;
     }
        
