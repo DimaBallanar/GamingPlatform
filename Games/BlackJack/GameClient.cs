4aed7fc29c5b5c895d4264a6eda331e9e6f47c04
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-06 15:25:49 +0300

    update

--- Games/BlackJack/GameClient.cs
+++ Games/BlackJack/GameClient.cs
@@ -7,7 +7,81 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
-    internal class GameClient
+    public class GameClient
     {
+        private CardDeck deck = new();
+        private Player[] players;
+
+        public GameClient()
+        {
+            players = new[]
+            {
+             new Player("Дилер", deck),
+             new Player("Игрок", deck)
+         };
+
+            Start();
+        }
+
+        public bool IsEnded { get; private set; }
+
+        public void Start()
+        {
+            bool replay = true;
+            while (replay)
+            {
+                Console.Clear();
+                Turn();
+                IsEnded = players.Any(x => x.IsLost);
+
+                Console.ReadLine();
+
+                if (IsEnded)
+                    replay = Replay();
+            }
+        }
+
+        private void Turn()
+        {
+            foreach (var player in players)
+            {
+                var status = player.Take() ? "Еще в игре" : "Проиграл";
+                Console.WriteLine($"У {player.Name} {player.Score}, он {status}");
+            }
+        }
+
+        private bool Replay()
+        {
+            bool result = false;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Повторить? (y/n): ");
+                var value = Console.ReadLine();
+
+                if (value is "y" or "n")
+                {
+                    result = value is "y";
+                    break;
+                }
+            }
+
+            if (result)
+            {
+                Reset();
+            }
+
+            return result;
+        }
+
+        private void Reset()
+        {
+            deck = new();
+            foreach (var player in players)
+            {
+                player.Reset();
+            }
+        }
     }
 }
