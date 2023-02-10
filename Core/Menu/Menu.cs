82661655e6b9192d6109b9c40f6033432d627d48
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-11 00:05:34 +0300

    update

--- Core/Menu/Menu.cs
+++ Core/Menu/Menu.cs
@@ -9,5 +9,18 @@ namespace Core.Menu
     public static class Menu
     {
 
+        public string Login()
+        {
+            System.Console.Write("введите логин: ");
+            string? login = Console.ReadLine();
+            System.Console.Write("введите пароль:");
+            string? pass = Console.ReadLine();
+            return login;
+        }
+
+        public string Password()
+        {
+
+        }
     }
 }
--- Core/Repo/UserRepo.cs
+++ Core/Repo/UserRepo.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Models;
 
 namespace Core.Repo
 {
-    internal class UserRepo
+    public  class UserRepo
     {
+        
+       private string[] Lines = File.ReadAllLines(@"MyDir\DT.txt");
+        public UserRepo() { }
+
+
+        private bool Search(string? login, string? pass)
+        {
+            
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(pass))
+            {
+                Console.WriteLine("ERROR");
+                return false;
+            }
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                string[] strings = Lines[i].Split(',');
+                if (strings[0] == login && strings[1] == pass)
+                {
+                    Console.WriteLine("Login Succesful");
+                    return true;
+                }
+            }
+            System.Console.WriteLine("ERROR input");
+            return false;
+        }
     }
 }
