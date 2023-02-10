d6678d05c59156c35cbc5892a5d304fc7c8863f6
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-10 22:48:16 +0300

    update

---
+++ Games/BlackJack/CardDeck.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class CardDeck
+    {
+        private readonly List<Card> cards = new();
+        public CardDeck() => GenerateCards();
+
+        public Card Take()
+        {
+            if (IsEmpty) return null;
+            var card = cards[new Random().Next(cards.Count)];
+            cards.Remove(card);
+
+            return card;
+        }
+
+        public bool IsEmpty => !cards.Any();
+        public int Count => cards.Count;
+
+        private void GenerateCards()
+        {
+            foreach (var type in Enum.GetValues<CardEnum>())
+            {
+                if (type is CardEnum.Number)
+                {
+                    for (int i = 2; i <= 9; i++)
+                    {
+                        cards.AddRange(CreateCardsByType(type, i));
+                    }
+                }
+                else
+                {
+                    cards.AddRange(CreateCardsByType(type));
+                }
+            }
+        }
+
+        private IEnumerable<Card> CreateCardsByType(CardEnum type, int value = 0)
+        {
+            List<Card> result = new();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Card card = type switch
+                {
+                    CardEnum.Number => new($"{value}", value, type),
+                    CardEnum.Jack => new($"Валет", 10, type),
+                    CardEnum.Queen => new($"Дама", 10, type),
+                    CardEnum.King => new($"Король", 10, type),
+                    CardEnum.Ace => new($"Туз", 11, type),
+                };
+
+                result.Add(card);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Games/BlackJack/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack;
+
+internal class Player
+{
+    private CardDeck deck;
+    private List<Card> cards = new();
+
+    public Player(string name, CardDeck deck)
+        => (Name, this.deck) = (name, deck);
+
+    public string Name { get; init; }
+    public int Money { get; private set; } = 1000;
+    public int Score { get; private set; }
+    public int Cards => cards.Count;
+    public bool IsLost => Score > 21;
+
+    public bool Take()
+    {
+        if (IsLost) return false;
+        var card = deck.Take();
+        cards.Add(card);
+        Score += card.Value;
+        return !IsLost;
+    }
+
+    public void Reset()
+    {
+        Score = 0;
+        cards.Clear();
+    }
+}
