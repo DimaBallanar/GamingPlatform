d11cc6d2058c11c8899c7da4975533b8a926f0f5
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-17 21:42:05 +0300

    ip

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Core.Menu;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menus opr = new Menus();
+            opr.FirstMenu();
+
+        }
+    }
+}
